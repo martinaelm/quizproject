0063d0a8fb77772dd31d2a450186d8b6625d2924
martinaelm <martina@tripbirds.com>
2017-09-27 12:29:33 +0200

    har lagt in test-quizzen

--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 def startpage(request):
-	return render(request, "startpage.html")
+	context = {
+		"quizzes": quizzes,	
+
+	}
+	return render(request, "startpage.html", context)
 
 def quiz(request, quiz_number):
 	return render(request, "quiz.html")
@@ -11,3 +15,22 @@ def question(request, quiz_number, question_number):
 
 def completed(request, quiz_number):
 	return render(request, "completed.html")
+
+
+quizzes = [
+	{
+		"quiz_number": 1,
+		"name": "Klassiska böcker",
+		"description": "Hur bra kan du dina klassiker?"
+	},
+	{
+		"quiz_number": 2,
+		"name": "Största fotbollslagen",
+		"description": "Kan du dina lag?"
+	},
+	{
+		"quiz_number": 3,
+		"name": "Världens mest kända hackare",
+		"description": "Kan du din hackerhistoria?"
+	},
+]
