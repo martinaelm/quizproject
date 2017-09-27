fdfe6a99baed69aae9459401ca1776110421b6ee
martinaelm <martina@tripbirds.com>
2017-09-27 12:03:50 +0200

    har uppdaterat url:er

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from quiz import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	path("", views.startpage),
+	path("quiz/<int:quiz_number>/", views.quiz),
+	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question),
+	path("quiz/<int:quiz_number>/completed/", views.completed),
 ]
