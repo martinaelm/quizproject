c4f985341c16d94649ee369c5d383cfe15775372
martinaelm <martina@tripbirds.com>
2017-09-28 10:37:46 +0200

    har lagt till paths i urls

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.urls import path
 from quiz import views
 
+from django.contrib import admin
+
 urlpatterns = [
 	path("", views.startpage, name="start_page"),
 	path("quiz/<int:quiz_number>/", views.quiz, name="quiz_page"),
 	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name="question_page"),
 	path("quiz/<int:quiz_number>/completed/", views.completed, name="completed_page"),
+	path('admin/', admin.site.urls),
 ]
