4b3a0c77ae7d1274343e971b18e1a84f8611f689
martinaelm <martina@tripbirds.com>
2017-09-28 11:03:24 +0200

    såg till att man kan se namn på quiz i gränsnitt

--- quiz/models.py
+++ quiz/models.py
@@ -5,6 +5,9 @@ class Quiz(models.Model):
 	name = models.CharField(max_length=100)
 	description = models.TextField()
 
+	def __str__(self):
+		return self.name 
+
 class Question(models.Model):
 	question = models.TextField()
 	answer1 = models.CharField(max_length=100)
@@ -12,3 +15,4 @@ class Question(models.Model):
 	answer3 = models.CharField(max_length=100)
 	correct = models.PositiveIntegerField()
 	quiz = models.ForeignKey(Quiz, related_name="questions", on_delete=models.CASCADE)
+
